a3e0e3389a9b595395ddd8ada66309c97217f713
Jeff Inman <jeffery.inman@hotmail.com>
2021-11-13 16:19:13 -0800

    started inventory

--- src/App.js
+++ src/App.js
@@ -40,9 +40,6 @@ class App extends React.Component {
   }
 
   render() {
-    console.log('app.js - zip: ', this.state.zipCode);
-    console.log('app.js - user ', this.state.user);
-
     return (
       <>
 
