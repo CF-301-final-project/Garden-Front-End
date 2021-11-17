8de47da03397a09cdba393d8af13cda5eba4588a
Ian Cargill <idcargill@gmail.com>
2021-11-17 08:51:30 -0800

    update plant display

--- src/Components/PlantDetails.js
+++ src/Components/PlantDetails.js
@@ -4,16 +4,21 @@ import Card from 'react-bootstrap/Card';
 
 class PlantDetails extends React.Component{
   render() {
+
     const { plantName, daysToMaturity, plantFamily, lightRequirements, determinate, directSowDate, plantDescription } = this.props.plant;
+
+    const d = new Date(directSowDate);
+    const date = d.toLocaleDateString();
+
     return (
       <>
       {plantName && (<Card style={{ maxWidth: '20rem', maxHeight: '600px', minHeight: '600px'}} className='overflow-auto'>
           <Card.Title>{plantName}</Card.Title>
-          <Card.Subtitle>{plantFamily}</Card.Subtitle>
-          <Card.Text>{daysToMaturity}</Card.Text>
-          <Card.Text>{lightRequirements}</Card.Text>
-          <Card.Text>Date Planted: {directSowDate}</Card.Text>
-          <Card.Text>Determinate: {determinate}</Card.Text>
+          <Card.Subtitle>Family: {plantFamily}</Card.Subtitle>
+          <Card.Text>Date Planted: {date}</Card.Text>
+          <Card.Text>Days to Mature: {daysToMaturity}</Card.Text>
+          <Card.Text>Lighting: {lightRequirements}</Card.Text>
+          <Card.Text>{determinate}</Card.Text>
           <Card.Body className='overflow-auto'>Details: {plantDescription}</Card.Body>
 
         </Card>)
